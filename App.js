39f68ae6d64be4dd3ba25ff50ae19695452f0e7f
Wilson Ndirangu <wndirangu@lastmile-networks.net>
2016-10-28 21:52:55 +0300

    added state

--- App.js
+++ App.js
@@ -3,17 +3,63 @@
  * @Date:   2016-10-28T19:09:57+03:00
  * @Email:  wnwillyndirangu@gmail.com
 * @Last modified by:   wndirangu
-* @Last modified time: 2016-10-28T20:59:02+03:00
+* @Last modified time: 2016-10-28T21:52:24+03:00
  * @License: MIT
  */
 
 import React from 'react';
+import ReactDOM from 'react-dom';
 
 class App extends React.Component {
     render() {
-        return <div> Hello  Me</div>
+        let txt = this.props.txt
+        return <h1 > {
+            txt
+        } < /h1>
 
     }
 }
 
-export default App
+App.propTypes = {
+    txt: React.PropTypes.string.isRequired,
+    cat: React.PropTypes.number
+}
+
+App.defaultProps = {
+    txt: "This is the default props"
+}
+
+ReactDOM.render( < App txt = "This is me doing ma thing" / > , document.getElementById('App'))
+
+class State extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            txt: "this is the state"
+        }
+
+    }
+    update(e) {
+
+        this.setState({
+            txt: e.target.value
+        })
+
+
+    }
+    render() {
+        return ( < div >
+            <
+            input type = "text"
+            onChange = {
+                this.update.bind(this)
+            }
+            /> <
+            h1 > {
+                this.state.txt
+            } < /h1> < /
+            div >
+        );
+    }
+}
+export default State
--- main.js
+++ main.js
@@ -3,10 +3,12 @@
  * @Date:   2016-10-28T19:11:03+03:00
  * @Email:  wnwillyndirangu@gmail.com
 * @Last modified by:   wndirangu
-* @Last modified time: 2016-10-28T20:47:28+03:00
+* @Last modified time: 2016-10-28T21:51:40+03:00
  * @License: MIT
  **/
-import React from 'react';
-import ReactDOM from 'react-dom';
+ import React from 'react';
 import App from './App';
-ReactDOM.render( <App /> , document.getElementById('App'))
+import State from './App';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(<State/>,document.getElementById('State'))
